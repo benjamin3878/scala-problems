c746dfa254bd979e0e4b973b40ad13c4b7176663
Ben Poling <benjamin3878@gmail.com>
2020-05-25 15:13:49 +0000

    added alternative methods for FindDuplicatesInArray

--- src/main/scala/array/FindDuplicatesInArray.scala
+++ src/main/scala/array/FindDuplicatesInArray.scala
@@ -1,20 +1,41 @@
 package array
 
+import scala.collection.mutable
 import scala.math.Ordering
 
 /**
-  * How do you find the duplicates a given array?
-  */
+ * How do you find the duplicates a given array?
+ */
 object FindDuplicatesInArray {
 
-  def apply[A: Ordering](arr: Array[A]): Set[A] = {
-    if (arr.isEmpty) { Set.empty }
-    else {
-      val sortedArr = arr.sorted
-      sortedArr.drop(1).foldLeft((Set.empty[A], sortedArr.head)) {
-        case ((dups, last), cur) if cur == last =>  dups + cur -> last
-        case ((dups, _), cur) => dups -> cur
-      }._1
+  /*
+   * O(n^2) compute
+   * O(1) memory
+   */
+  def computationallyExpensive[A](arr: Array[A]): Set[A] =
+    (for {
+      (ele, i) <- arr.view.zipWithIndex
+      next <- (i + 1 until arr.length).map(arr)
+      if (ele == next)
+    } yield { next }).toSet
+
+  def memoryExpensive[A](arr: Array[A]): Set[A] = {
+    val map = mutable.Map.empty[A, Int]
+
+    arr.foreach {
+      case ele if map.contains(ele) => map += ele -> (map(ele) + 1)
+      case ele => map += (ele -> 1)
     }
+
+    map.filter(_._2 > 1).keys.toSet
   }
+
+  def sortAndCompareLast[A: Ordering](arr: Array[A]): Set[A] =
+    arr.sorted.foldLeft((Set.empty[A], Option.empty[A])) {
+      case ((acc, None), next) => (acc, Some(next))
+      case ((acc, Some(last)), next) if last == next => (acc + next, Some(next))
+      case ((acc, _), next) => (acc, Some(next))
+    }._1
+
+  def scalaStyle[A](arr: Array[A]): Set[A] = arr.groupBy(identity).collect { case (ele, ls) if ls.length > 1 => ele }.toSet
 }
--- src/test/scala/array/FindDuplicatesInArraySpec.scala
+++ src/test/scala/array/FindDuplicatesInArraySpec.scala
@@ -1,41 +1,51 @@
 package array
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.WordSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.math.Ordering
 
 class FindDuplicatesInArraySpec extends WordSpec with Matchers {
 
+  def allFindDuplicatesInArrayMethod[A: Ordering]: List[Array[A] => Set[A]] = List(
+    FindDuplicatesInArray.computationallyExpensive[A],
+    FindDuplicatesInArray.memoryExpensive[A],
+    FindDuplicatesInArray.sortAndCompareLast[A],
+    FindDuplicatesInArray.scalaStyle[A]
+  )
+
   "FindDuplicatesInArray" should {
     "return an empty set for an empty array" in {
-      FindDuplicatesInArray(Array.empty[Int]) shouldBe Set.empty[Int]
+      allFindDuplicatesInArrayMethod[Int].foreach(_.apply(Array.empty[Int]) shouldBe Set.empty[Int])
     }
     "return an empty set for an array of one element" in {
-      FindDuplicatesInArray(Array(1)) shouldBe Set.empty[Int]
-      FindDuplicatesInArray(Array("1")) shouldBe Set.empty[String]
-      FindDuplicatesInArray(Array(1.0)) shouldBe Set.empty[Double]
-      FindDuplicatesInArray(Array(1L)) shouldBe Set.empty[Long]
+      allFindDuplicatesInArrayMethod[Int].foreach(_.apply(Array(1)) shouldBe Set.empty[Int])
+      allFindDuplicatesInArrayMethod[String].foreach(_.apply(Array("1")) shouldBe Set.empty[String])
+      allFindDuplicatesInArrayMethod[Double].foreach(_.apply(Array(1.0)) shouldBe Set.empty[Double])
+      allFindDuplicatesInArrayMethod[Long].foreach(_.apply(Array(1L)) shouldBe Set.empty[Long])
     }
     "find the one duplicate" in {
       val arr = Array(1, 1)
       val set = Set(1)
-      FindDuplicatesInArray(arr) shouldBe set
-      FindDuplicatesInArray(arr.map(_.toString)) shouldBe set.map(_.toString)
-      FindDuplicatesInArray(arr.map(_.toDouble)) shouldBe set.map(_.toDouble)
-      FindDuplicatesInArray(arr.map(_.toLong)) shouldBe set.map(_.toLong)
+      allFindDuplicatesInArrayMethod[Int].foreach(_.apply(arr) shouldBe set)
+      allFindDuplicatesInArrayMethod[String].foreach(_.apply(arr.map(_.toString)) shouldBe set.map(_.toString))
+      allFindDuplicatesInArrayMethod[Double].foreach(_.apply(arr.map(_.toDouble)) shouldBe set.map(_.toDouble))
+      allFindDuplicatesInArrayMethod[Long].foreach(_.apply(arr.map(_.toLong)) shouldBe set.map(_.toLong))
 
       val arr2 = Array(2, 1, 1)
 
-      FindDuplicatesInArray(arr2) shouldBe set
-      FindDuplicatesInArray(arr2.map(_.toString)) shouldBe set.map(_.toString)
-      FindDuplicatesInArray(arr2.map(_.toDouble)) shouldBe set.map(_.toDouble)
-      FindDuplicatesInArray(arr2.map(_.toLong)) shouldBe set.map(_.toLong)
+      allFindDuplicatesInArrayMethod[Int].foreach(_.apply(arr2) shouldBe set)
+      allFindDuplicatesInArrayMethod[String].foreach(_.apply(arr2.map(_.toString)) shouldBe set.map(_.toString))
+      allFindDuplicatesInArrayMethod[Double].foreach(_.apply(arr2.map(_.toDouble)) shouldBe set.map(_.toDouble))
+      allFindDuplicatesInArrayMethod[Long].foreach(_.apply(arr2.map(_.toLong)) shouldBe set.map(_.toLong))
     }
     "find all of the duplicates" in {
       val arr = Array(2, 1, 1, 3, 4, 4, 5)
       val set = Set(1,4)
-      FindDuplicatesInArray(arr) shouldBe set
-      FindDuplicatesInArray(arr.map(_.toString)) shouldBe set.map(_.toString)
-      FindDuplicatesInArray(arr.map(_.toDouble)) shouldBe set.map(_.toDouble)
-      FindDuplicatesInArray(arr.map(_.toLong)) shouldBe set.map(_.toLong)
+      allFindDuplicatesInArrayMethod[Int].foreach(_.apply(arr) shouldBe set)
+      allFindDuplicatesInArrayMethod[String].foreach(_.apply(arr.map(_.toString)) shouldBe set.map(_.toString))
+      allFindDuplicatesInArrayMethod[Double].foreach(_.apply(arr.map(_.toDouble)) shouldBe set.map(_.toDouble))
+      allFindDuplicatesInArrayMethod[Long].foreach(_.apply(arr.map(_.toLong)) shouldBe set.map(_.toLong))
     }
   }
 
